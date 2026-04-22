585a49dddffff9c94b97f987ad41b9a653eb3023
antonbulgakov <bulgakov.anton.a@gmail.com>
2026-04-22 12:45:28 +0300

    WRS-0001 - fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,12 @@ repositories {
 }
 
 dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
 
 tasks.test {
     useJUnitPlatform()
